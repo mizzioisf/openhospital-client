16757e07530d79ff712543591f25615b6db55cc9
mizzio <mizzio@informaticisenzafrontiere.org>
2021-04-30 14:24:06 +0200

    test

--- oh.ps1
+++ oh.ps1
@@ -428,8 +428,13 @@ function inizialize_database {
 function start_database {
 	Write-Host "Starting MySQL server... "
 	try {
-		Start-Process -FilePath "$OH_PATH\$MYSQL_DIR\bin\mysqld.exe" -ArgumentList ("--defaults-file=$([char]34)$OH_PATH\etc\mysql\my.cnf$([char]34) --tmpdir=$OH_PATH\$TMP_DIR --standalone") -NoNewWindow -RedirectStandardOutput "$LOG_DIR/$LOG_FILE" -RedirectStandardError "$LOG_DIR/$LOG_FILE_ERR"
-		sleep 2;
+	STARTCMDMYSQL=@"
+        --defaults-file=$OH_PATH\etc\mysql\my.cnf --tmpdir=$OH_PATH\$TMP_DIR --standalone"
+"@
+#Y		Start-Process -FilePath "$OH_PATH\$MYSQL_DIR\bin\mysqld.exe" -ArgumentList ("--defaults-file=$([char]34)$OH_PATH\etc\mysql\my.cnf$([char]34) --tmpdir=$OH_PATH\$TMP_DIR --standalone") -NoNewWindow -RedirectStandardOutput "$LOG_DIR/$LOG_FILE" -RedirectStandardError "$LOG_DIR/$LOG_FILE_ERR"
+#		Start-Process -FilePath "$OH_PATH\$MYSQL_DIR\bin\mysqld.exe" -ArgumentList ("$STARTCMDMYSQL") -NoNewWindow -RedirectStandardOutput "$LOG_DIR/$LOG_FILE" -RedirectStandardError "$LOG_DIR/$LOG_FILE_ERR"
+		Start-Process -FilePath "$OH_PATH\$MYSQL_DIR\bin\mysqld.exe" -ArgumentList ("$STARTCMDMYSQL") -NoNewWindow 
+		sleep 2; read-host;
 	}
 	catch {
 		Write-Host "Error: Database not started! Exiting." -ForegroundColor Red
