a619c246acb3e88df3c69855e496e56a762a1e40
mizzio <mizzio@informaticisenzafrontiere.org>
2021-11-12 00:50:28 +0100

    fix

--- oh.sh
+++ oh.sh
@@ -115,7 +115,7 @@ esac
 ######## MySQL/MariaDB Software
 # MariaDB
 MYSQL_VERSION="10.2.41"
-MYSQL_URL="https://downloads.mariadb.com/MariaDB/mariadb-$MYSQL_VERSION/bintar-linux-$MYSQL_ARCH"
+MYSQL_URL="https://archive.mariadb.org/mariadb-$MYSQL_VERSION/bintar-linux-$MYSQL_ARCH"
 MYSQL_DIR="mariadb-$MYSQL_VERSION-linux-$MYSQL_PACKAGE_ARCH"
 # MySQL
 #MYSQL_URL="https://downloads.mysql.com/archives/get/p/23/file"
