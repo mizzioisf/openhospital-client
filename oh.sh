2a31bcce92c6ba79e82dccbd28a45c3f3c18afb8
mizzio <mizzio@informaticisenzafrontiere.org>
2021-11-15 23:14:31 +0100

    fix

--- oh.sh
+++ oh.sh
@@ -551,11 +551,9 @@ function generate_config_files {
 	./$OH_DIR/rsc/database.properties.dist > ./$OH_DIR/rsc/database.properties
 
 	######## settings.properties setup
-	# set language in OH config file
+	# set language and DOC_DIR in OH config file
 	[ -f ./$OH_DIR/rsc/settings.properties ] && mv -f ./$OH_DIR/rsc/settings.properties ./$OH_DIR/rsc/settings.properties.old
-	sed -e "s/OH_LANGUAGE/$OH_LANGUAGE/g" ./$OH_DIR/rsc/settings.properties.dist > ./$OH_DIR/rsc/settings.properties
-	# set DOC_DIR in OH config file
-	sed -e "s/OH_DOC_DIR/$OH_DOC_DIR/g" ./$OH_DIR/rsc/settings.properties.dist > ./$OH_DIR/rsc/settings.properties
+	sed -e "s/OH_LANGUAGE/$OH_LANGUAGE/g" -e "s/OH_DOC_DIR/$OH_DOC_DIR/g" ./$OH_DIR/rsc/settings.properties.dist > ./$OH_DIR/rsc/settings.properties
 }
 
 
