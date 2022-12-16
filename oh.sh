0dd7ee8450667c9ed79ed3bf88b17937d749b7e7
mizzio <mizzio@informaticisenzafrontiere.org>
2022-12-16 03:15:34 +0100

    Update

--- oh.sh
+++ oh.sh
@@ -1038,8 +1038,8 @@ fi
 echo "Generate config files is set to $GENERATE_CONFIG_FILES"
 echo "Starting Open Hospital in $OH_MODE mode..."
 echo "OH_PATH is set to $OH_PATH"
-if [ -z "$OH_LANGUAGE" ]; then echo "OH language is set to $OH_LANGUAGE"; fi
-if [ -z "$LOG_LEVEL" ]; then echo "OH log level is set to $LOG_LEVEL"; fi
+if [ -n "$OH_LANGUAGE" ]; then echo "OH language is set to $OH_LANGUAGE"; fi
+if [ -n "$LOG_LEVEL" ]; then echo "OH log level is set to $LOG_LEVEL"; fi
 
 # check for java
 java_check;
