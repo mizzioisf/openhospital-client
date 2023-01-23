0354fbd74dd12d2d1882fd389a4a118fe0552c9b
mizzio <mizzio@informaticisenzafrontiere.org>
2023-01-23 19:56:54 +0100

    Symbolic link update

--- oh.ps1
+++ oh.ps1
@@ -927,7 +927,7 @@ if ( $INTERACTIVE_MODE -eq "on" ) {
 	$WshShell = New-Object -comObject WScript.Shell
 #	$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\supercoolprogram\mrincredible.lnk")
 	$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\OpenHospital.lnk")
-	$Shortcut.TargetPath = "SCRIPT_DIR/$SCRIPT_NAME"
+	$Shortcut.TargetPath = "$SCRIPT_DIR/$SCRIPT_NAME"
 	$Shortcut.Arguments = "-mode $OH_MODE -lang $OH_LANGUAGE"
 	$Shortcut.WorkingDirectory = "$OH_DIR"
 	$Shortcut.Save()
