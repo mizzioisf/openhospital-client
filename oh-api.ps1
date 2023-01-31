3ba13b1de9f5a517eaa844abbdfbce2dd5da4bbf
mizzio <mizzio@informaticisenzafrontiere.org>
2023-01-31 23:49:18 +0100

    fix

--- oh-api.ps1
+++ oh-api.ps1
@@ -898,7 +898,7 @@ function start_api {
 
 	$JAVA_ARGS="-client -Xms64m -Xmx1024m -cp ./bin/openhospital-api-0.0.2.jar;./rsc;./static org.springframework.boot.loader.JarLauncher"
 
-	Start-Process -FilePath "$JAVA_BIN" -ArgumentList $JAVA_ARGS -Wait -NoNewWindow -RedirectStandardOutput "$OH_DIR/$LOG_DIR/$API_LOG_FILE" -RedirectStandardError "$OH_DIR/$LOG_DIR/$API_ERR_LOG_FILE"
+	Start-Process -FilePath "$JAVA_BIN" -ArgumentList $JAVA_ARGS -Wait -NoNewWindow -RedirectStandardOutput "$OH_PATH/$LOG_DIR/$API_LOG_FILE" -RedirectStandardError "$OH_PATH/$LOG_DIR/$API_ERR_LOG_FILE"
 
         # $JAVA_BIN -client -Xms64m -Xmx1024m -cp "./bin/openhospital-api-0.0.2.jar:./rsc::./static" org.springframework.boot.loader.JarLauncher
 
