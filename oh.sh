d8918a46b7ffde6c0129a9cf928079b9061c0702
mizzio <mizzio@informaticisenzafrontiere.org>
2023-04-18 00:42:28 +0200

    Addedd sql workaround

--- oh.sh
+++ oh.sh
@@ -822,7 +822,7 @@ function start_api_server {
 	# workaround for hard coded password limit
 	echo "Setting admin password..."
 	cd "./$SQL_EXTRA_DIR/"
-	../$MYSQL_DIR/bin/mysql --local-infile=1 -u root -p$DATABASE_ROOT_PW --host=$DATABASE_SERVER --port=$DATABASE_PORT --protocol=tcp $DATABASE_NAME < ./reset_admin_password_strong.sql >> ../$LOG_DIR/$LOG_FILE 2>&1
+	../$MYSQL_DIR/bin/mysql --local-infile=1 -u root -p$DATABASE_ROOT_PW --host=$DATABASE_SERVER --port=$DATABASE_PORT --protocol=tcp $DATABASE_NAME < ./reset_admin_password_strong.sql >> ../../$LOG_DIR/$LOG_FILE 2>&1
 	if [ $? -ne 0 ]; then
 		echo "Error! Exiting."
 		shutdown_database;
