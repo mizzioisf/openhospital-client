d528e3101f3a5e1b9afc71f03ee12ba33ce87de3
mizzio <mizzio@informaticisenzafrontiere.org>
2025-01-14 14:58:45 +0100

    Year bump

--- oh.ps1
+++ oh.ps1
@@ -2,7 +2,7 @@
 #!/usr/bin/pwsh
 #
 # Open Hospital (www.open-hospital.org)
-# Copyright © 2006-2024 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+# Copyright © 2006-2025 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
 #
 # Open Hospital is a free and open source software for healthcare data management.
 #
