9b2ffebd15d6a829795f6228c8c8911ff5461738
mizzioisf <mizzio@informaticisenzafrontiere.org>
2026-09-24 19:09:34 +0200

    2026.09.24 update

--- oh/oh-ui/assets/PatientDetailsActivityContent-B69zFOf-.js
+++
@@ -1,1 +0,0 @@
-import{j as t,r as a}from"./index-CcHW82bx.js";const i=({title:e,children:s})=>t.jsxs(a.Fragment,{children:[t.jsx("div",{className:"patientDetails__content_header",children:t.jsx("h3",{children:e})}),t.jsx("div",{className:"patientDetails__content_body",children:s})]});export{i as P};
